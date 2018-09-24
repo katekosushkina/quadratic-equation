c56b17df0867509b3fe825b6fc8cf80e9f1a2af6
katekosushkina <42875091+katekosushkina@users.noreply.github.com>
2018-09-24 17:57:12 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    let mass = equation.split(' ');
+    let a = parseInt(mass[0]);
+    let b = parseInt(mass[3]+mass[4]);
+    let c = parseInt(mass[7]+mass[8]);;
+    let d = b*b-4*a*c;
+    let x1 = Math.round((-b+Math.sqrt(d))/(2*a));
+    let x2 = Math.round((-b-Math.sqrt(d))/(2*a));
+    let sol = [x1, x2];
+    sol.sort(function(a, b) {
+      return a - b;
+    });
+    return sol;
+  }
